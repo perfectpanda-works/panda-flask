258ce41945581c47c928c27ff16a9fb2e05ca476
perfectpanda <perfectpanda@hotmail.co.jp>
2020-10-01 00:31:18 +0900

    customized filter test

--- min_app/__init__.py
+++ min_app/__init__.py
@@ -3,4 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', data=["1","2","3"])
+
+@app.template_filter('reverse')
+def reverse_filter(s):
+    return s[::-1]
